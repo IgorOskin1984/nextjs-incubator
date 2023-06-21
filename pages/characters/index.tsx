38b0a9ba575c07cf23b25dbfd7ad6d7eaf07b548
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-21 19:33:31 +0300

    не ругается а все из-за СКОБОК!!! в мапе

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,37 +1,37 @@
-import Image from "next/image"
-import { API } from "../../assets/api/api"
-import { CharacterType, ResponseType } from "../../assets/api/rick-and-morty-api"
-import { Header } from "../../components/Header/Header"
-import { PageWrapper } from "../../components/PageWrapper/PageWrapper"
+import Image from "next/image";
+import { API } from "../../assets/api/api";
+import { CharacterType, ResponseType } from "../../assets/api/rick-and-morty-api";
+import { Header } from "../../components/Header/Header";
+import { PageWrapper } from "../../components/PageWrapper/PageWrapper";
 
 export const getStaticProps = async () => {
-	const characters = await API.rickAndMorty.getCharacters()
+	const characters = await API.rickAndMorty.getCharacters();
 
 	return {
 		props: {
 			characters
 		}
-	}
-}
+	};
+};
 
 type PropsType = {
-	characters: ResponseType<CharacterType>
-}
+	characters: ResponseType<CharacterType>;
+};
 
 const Characters = (props: PropsType) => {
+	const { characters } = props;
 
-	const { characters } = props
-
-	const charactersList = characters.results.map(charr => {
+	const charactersList = characters.results.map((charr) => (
 		<div key={charr.id}>{charr.name}</div>
-	})
+	));
+
 	return (
 		<PageWrapper>
 			<Header />
 			<p>Characters</p>
 			{charactersList}
 		</PageWrapper>
-	)
-}
+	);
+};
 
-export default Characters
+export default Characters;
