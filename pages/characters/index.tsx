14dc05dddd8038d85303c2f72f05fc9fb78f09bb
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-21 19:16:49 +0300

    пишем Characters и getStaticProps

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,8 +1,39 @@
-const Characters = () => {
+import Image from "next/image"
+import { API } from "../../assets/api/api"
+import { CharacterType, ResponseType } from "../../assets/api/rick-and-morty-api"
+import { Header } from "../../components/Header/Header"
+import { PageWrapper } from "../../components/PageWrapper/PageWrapper"
+
+export const getStaticProps = async () => {
+	const characters = await API.rickAndMorty.getCharacters()
+
+	return {
+		props: {
+			characters
+		}
+	}
+}
+
+type PropsType = {
+	characters: ResponseType<CharacterType>
+}
+
+const Characters = (props: PropsType) => {
+	const { characters } = props
 	return (
-		<>
-			<p>Characters</p>
-		</>
+		<div>
+			<PageWrapper>
+				<Header />
+				<Image
+					src="/next.svg"
+					alt="Next.js Logo"
+					width={180}
+					height={37}
+					priority
+				/>
+				<p>Characters</p>
+			</PageWrapper>
+		</div>
 	)
 }
 
